30324fab2f5a90a6bc57a49607a71cc867cf5969
AugustinPech <augustin.pech.alcoba@gmail.com>
2024-07-29 18:43:16 +0200

    router have 2 routes for now : home and 404

--- src/Router/Router.php
+++ src/Router/Router.php
@@ -6,7 +6,7 @@ class Router {
     ];
     public function route() {
         $requestedRoute = filter_input(INPUT_GET, 'route', FILTER_SANITIZE_SPECIAL_CHARS);
-        $requestedRoute = isset($requestedRoute) ? (array_key_exists($requestedRoute, $routes) ? $requestedRoute : '404' ) : 'home';
-        return $routes[$requestedRoute];
+        $requestedRoute = isset($requestedRoute) ? (array_key_exists($requestedRoute, $this->routes) ? $requestedRoute : '404' ) : 'home';
+        return $this->routes[$requestedRoute];
     }
 }
