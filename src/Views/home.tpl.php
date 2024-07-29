65b3123a9b19ac56fe0a08eaeeee9a7c9758fb6b
AugustinPech <augustin.pech.alcoba@gmail.com>
2024-07-29 18:57:23 +0200

    added a view

---
+++ src/Views/home.tpl.php
@@ -0,0 +1,19 @@
+<?php
+$metatitle = 'HomePage';
+$metadescription = 'This is the homepage of the website';
+$metakeywords = 'homepage, home, page';
+
+// TODO : include Head
+// TODO : include Header
+
+?>
+
+<main>
+    <h1>Home Page</h1>
+    <p>Welcome to the homepage of the website.</p>
+</main>
+
+<?php
+// TODO : include Footer
+
+?>
