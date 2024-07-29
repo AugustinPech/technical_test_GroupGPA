4031fdabc7159cf3dce1e7a70ad15dbdb7be314d
AugustinPech <augustin.pech.alcoba@gmail.com>
2024-07-29 18:40:52 +0200

    created a router

---
+++ src/Router/Router.php
@@ -0,0 +1,12 @@
+<?php
+class Router {
+    private $routes = [
+        'home' => '../src/Controllers/HomeController.php',
+        '404' => '../src/Views/Errors/404.php'
+    ];
+    public function route() {
+        $requestedRoute = filter_input(INPUT_GET, 'route', FILTER_SANITIZE_SPECIAL_CHARS);
+        $requestedRoute = isset($requestedRoute) ? (array_key_exists($requestedRoute, $routes) ? $requestedRoute : '404' ) : 'home';
+        return $routes[$requestedRoute];
+    }
+}
