26465d248228c8a9a65f73d2d9dabf725265e746
AugustinPech <augustin.pech.alcoba@gmail.com>
2024-07-30 12:01:25 +0200

    enhanced the view for the home page to show a list of categories

---
+++ src/Views/home.tpl.php
@@ -0,0 +1,21 @@
+<?php
+echo 'home.tpl.php <br>';
+$metatitle = 'HomePage';
+$metadescription = 'This is the homepage of the website';
+$metakeywords = 'homepage, home, page';
+
+// TODO : include Head
+// TODO : include Header
+?>
+
+<div>
+    <h1>Cool Home Page</h1>
+    <p>Welcome to the homepage of the website.</p>
+    <p>Here are the categories of products available:</p>
+    <ul>
+        <?php
+        foreach ($listCategories as $id => $name) : ?>
+            <li><a href="?route=category&id=<?= $id ?>"><?= $name ?></a></li>
+        <?php endforeach; ?>
+    </ul>
+</div>
