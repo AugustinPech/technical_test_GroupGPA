630858cb43257db39e42a21397b32b291549a6f4
AugustinPech <augustin.pech.alcoba@gmail.com>
2024-07-30 12:25:50 +0200

    added a method to the category model to get products from a category

--- src/Models/Category.php
+++ src/Models/Category.php
@@ -1,12 +1,18 @@
 <?php
 namespace App\Models;
 
-use App\Controllers\APIController;
+// use App\Controllers\APIController;
 
 class Category {
     public function listCategories() {
-        $api= new ApiController();
+        $api= new \App\Controllers\ApiController();
         $categories = $api->get('/products/categories');
         return $categories;
     }
+    public function listProductsByCategory($category) {
+        $api= new \App\Controllers\ApiController();
+        $products = $api->get('/products/category/'.$category);
+        return $products;
+    }
+        
 }
